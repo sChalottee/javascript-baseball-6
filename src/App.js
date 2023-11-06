f5f54216af033e0c264db57877fb9320bda32cf3
유종훈 <yujonghun@yujonghun-ui-MacBookAir.local>
2023-11-06 19:41:59 +0900

    테스트

--- src/App.js
+++ src/App.js
@@ -2,27 +2,36 @@ class App {
   async play() {}
 }
 
-const computer = [];
-while (computer.length < 3) {
-  const number = MissionUtils.Random.pickNumberInRange(1, 9);
-  if (!computer.includes(number)) {
-    computer.push(number);
-  }
-}
 class Game {
   constructor() {
     answer = "";
-    strike = 0:
+    strike = 0;
     ball = 0;
-    number = MissionUtils.Console.readLineAsync("숫자를 입력해주세요.")
+    number = MissionUtils.Console.readLineAsync("숫자를 입력해주세요.");
+    // number = [123]
+    computer = [];
   }
 
+  if (number.length !== 3) {
+  throw new Error("[ERROR]");
+  }
+
+  while (this.computer.length < 3) {
+  let number = MissionUtils.Random.pickNumberInRange(1, 9);
+  if (!computer.includes(Number(number))) {
+    computer.push(Number(number));
+  }
+}
   match() {
     for (let i = 0; i < computer.length; i++) {
-      computer[i] = number[i];
+      if (computer[i] = Number(number[i])) {
+        strike++;
+      } else if (computer.includes(Number(number[i]))) {
+        ball++;
+      }
     }
-
-    simpan() {
+  };
+  simpan() {
       if (strike === 3) {
         answer = `${strike}스트라이크 \n 3개의 숫자를 모두 맞히셨습니다! 게임 종료`
       } else if (strike === 0 && ball === 0) {
@@ -36,8 +45,13 @@ class Game {
       }
     }
   }
-}
+
 class Controller {
+  restart() {
+    const game = new Game();
+
+
+  }
 
 }
 export default App;
