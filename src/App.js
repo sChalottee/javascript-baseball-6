d129efd821a93d5e6c27fea1158ddc2029ac713d
유종훈 <yujonghun@yujonghun-ui-MacBookAir.local>
2023-11-16 22:27:56 +0900

    init: 초기화 후 코드 작성

--- src/App.js
+++ src/App.js
@@ -1,57 +1,57 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
-}
+    pickNumberArray;
+    choice;
 
-class Game {
-  constructor() {
-    answer = "";
-    strike = 0;
-    ball = 0;
-    number = MissionUtils.Console.readLineAsync("숫자를 입력해주세요.");
-    // number = [123]
-    computer = [];
-  }
-
-  if (number.length !== 3) {
-  throw new Error("[ERROR]");
-  }
-
-  while (this.computer.length < 3) {
-  let number = MissionUtils.Random.pickNumberInRange(1, 9);
-  if (!computer.includes(Number(number))) {
-    computer.push(Number(number));
-  }
-}
-  match() {
-    for (let i = 0; i < computer.length; i++) {
-      if (computer[i] = Number(number[i])) {
-        strike++;
-      } else if (computer.includes(Number(number[i]))) {
-        ball++;
-      }
+    constructor() {
+        this.pickNumberArray = [];
+        this.choice = 0;
     }
-  };
-  simpan() {
-      if (strike === 3) {
-        answer = `${strike}스트라이크 \n 3개의 숫자를 모두 맞히셨습니다! 게임 종료`
-      } else if (strike === 0 && ball === 0) {
-        answer = "낫싱"
-      } else if (strike !== 0 && ball === 0) {
-        answer = `${strike}스트라이크`
-      } else if (strike === 0 && ball !== 0) {
-        answer = `${ball}볼`
-      } else {
-        answer = `${strike}스트라이크 ${ball}볼`
-      }
+
+    async play() {
+        MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+        //랜덤 숫자 생성 함수
+        this.generateNum();
+        //숫자 입력
+        this.pickNumberArray = (
+            await MissionUtils.Console.readLineAsync("숫자를 입력해주세요 : ")
+        ).split(""); //[1,2,3]
+        //입력숫자 유효성검사
+        for (let yosu of this.pickNumberArray) {
+            if (isNaN(Number(yosu))) {
+                throw new Error("[ERROR]");
+            }
+        }
+        if (this.pickNumberArray.length !== 3) {
+            throw new Error("[ERROR]");
+        }
+
+        //스트 볼 판정 함수
+        this.strikeOrBall(this.pickNumberArray, this.generateN);
+        // MissionUtils.Console.print("1볼 1스트라이크");
+        this.choice = Number(
+            await MissionUtils.Console.readLineAsync(
+                "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+            )
+        );
+        //선택에 따라 재시작 or 끝
     }
-  }
 
-class Controller {
-  restart() {
-    const game = new Game();
-
-
-  }
-
+    //랜덤숫자 생성 함수
+    generateNum() {
+        let generateN = [];
+        while (generateN.length < 3) {
+            let number = MissionUtils.Random.pickNumberInRange(1, 9);
+            if (!generateN.includes(number)) {
+                generateN.push(number);
+            }
+        }
+        return generateN; //[1,2,3]
+    }
+    strikeOrBall(pickNumberArray, generateN) {
+        // pickNumberArray + generateN = [];
+    }
 }
+
 export default App;
